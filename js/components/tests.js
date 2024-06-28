8352727feab88558d0c49d45d87f0f646144625d
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-06-27 19:17:38 -0500

    feat: :sparkles: FUNCION DE ROCKETDETAILS
    Terminada la funcion que nos da toda info sobre los rockets(solo falta un porcentaje para colocar luego en el diseño css)

--- js/components/tests.js
+++ js/components/tests.js
@@ -60,7 +60,7 @@ const API_URL_STARLINK="https://https://api.spacexdata.com/v4/starlink";
 
 
 //HECHIZO PODEROSO DE INVOCACION DE LA INFO DE LOS ROCKETS EN BOTONES QUE AL PRESIONAR INVOCAS 
-const obtenerDatosCohete = async (rocketId) => {
+const getAllDetailDataRockets = async (rocketId) => {
     try {
         const response = await fetch(API_URL_ROCKETS); // formules lanza su encantamiento para buscar información mágica sobre cohetes
         const data = await response.json(); // formules decodifica la respuesta mágica en un pergamino JSON
@@ -69,102 +69,192 @@ const obtenerDatosCohete = async (rocketId) => {
         const rocket = data.find(rocket => rocket.id === rocketId);
         if (rocket) {
             const {
-                height: { meters: alturaMetros, feet: alturaPies },
-                diameter: { meters: diametroMetros, feet: diametroPies },
-                mass: { kg: masaKG, lb: masaLB },
+                height: {
+                    meters: meters_height,
+                    feet: feet_height
+                },
+                diameter: {
+                    meters: meters_diameter,
+                    feet: feet_diameter
+                },
+                mass: {
+                    kg: kg_mass,
+                    lb: lb_mass
+                },
                 first_stage: {
-                    thrust_sea_level: { kN: empujeNivelMar, lbf: empujeNivelMarLbf },
-                    reusable: reutilizablePrimeraEtapa,
-                    engines: cantidadMotores,
-                    fuel_amount_tons: cantidadCombustible,
-                    burn_time_sec: tiempoQuemadoPrimeraEtapa
+                    thrust_sea_level: {
+                        kN: kN_first_stage,
+                        lbf: lbf_first_stage
+                    },
+                    thrust_vacuum: {
+                        kN: kN_thrust_vacuum,
+                        lbf: lbf_thrust_vacuum
+                    },
+                    reusable: reusable_reusable,
+                    engines: engines_reusable,
+                    fuel_amount_tons: fuel_amount_tons_reusable,
+                    burn_time_sec: burn_time_sec_reusable
                 },
                 second_stage: {
-                    thrust: { kN: empujeSegundaEtapa, lbf: empujeVacio },
-                    payloads: { composite_fairing: { height: { meters: alturaCofia, feet: diametroCofia } } },
-                    reusable: reutilizableSegundaEtapa,
-                    engines: motoresSegundaEtapa,
-                    fuel_amount_tons: cantidadCombustibleSegundaEtapa,
-                    burn_time_sec: tiempoQuemadoSegundaEtapa
+                    thrust: {
+                        kN: kN_second_stage,
+                        lbf: lbf_second_stage
+                    },
+                    payloads: {
+                        composite_fairing: {
+                            height: {
+                                meters: meters_height_composite_fairing,
+                                feet: feet_height_composite_fairing
+                            },
+                            diameter: {
+                                meters: meters_diameter_second_stage,
+                                feet: feet_diameter_second_stage
+                            }
+                        },
+                        option_1: option_1
+                    },
+                    reusable: reusable,
+                    engines: engines,
+                    fuel_amount_tons: fuel_amount_tons,
+                    burn_time_sec: burn_time_sec
                 },
+                landing_legs: {
+                    number: number_landing_legs,
+                    material: material_landing_legs
+                },
+                payload_weights: [
+                    {
+                        id: payloadId,
+                        name: payloadName,
+                        kg: Kg_payload_weights,
+                        lb: lb_payload_weights
+                    }
+                ],
                 engines: {
-                    isp: { sea_level: empujeNivelMarMotor, vacuum: empujeVacíoMotor },
-                    type: tipoMotor,
-                    version: versiónMotor,
-                    layout: diseñoMotor,
-                    propellant_1: propulsante1,
-                    propellant_2: propulsante2
+                    isp: {
+                        sea_level: sea_level,
+                        vacuum: vacuum
+                    },
+                    thrust_sea_level: {
+                        kN: kN_sea,
+                        lbf: lbf_sea
+                    },
+                    thrust_vacuum: {
+                        kN: kN_vacuum,
+                        lbf: lbf_vacuum
+                    },
+                    number: number_engines,
+                    type: type_engine,
+                    version: version_engine,
+                    layout: single,
+                    engine_loss_max: engine_loss_max,
+                    propellant_1: propellant_1,
+                    propellant_2: propellant_2,
+                    thrust_to_weight: thrust_to_weight
                 },
-                payload_weights: [{ kg: pesoCargaLEO }],
-                flickr_images: [imagenFlickr1, imagenFlickr2],
-                name: nombreCohete,
-                type: tipoCohete,
-                active: activo,
-                stages: etapas,
-                boosters: impulsores,
-                cost_per_launch: costoLanzamiento,
-                success_rate_pct: tasaÉxito,
-                first_flight: primerVuelo,
-                country: país,
-                company: compañía,
-                wikipedia: enlaceWikipedia,
-                description: descripción,
-                id: idCohete
+                flickr_images: [
+                    imagenFlickr1, 
+                    imagenFlickr2
+                ],
+                name: nameRocket,
+                type: typeRocket,
+                active: active,
+                stages: stages,
+                boosters: boosters,
+                cost_per_launch: cost_per_launch,
+                success_rate_pct: success_rate_pct,
+                first_flight: first_flight,
+                country: country,
+                company: company,
+                wikipedia: wikipedia,
+                description: description,
+                id: idRocket
             } = rocket;
 
             console.log('Datos del cohete:', rocket); // Verifica los datos obtenidos
+
+            // Contar la cantidad de propelentes
+            const propellantCount = [propellant_1, propellant_2].filter(Boolean).length;
 // ______________________________________________________________________________________________________________________________________________________________________________//
 
+            // Imprimir dinámicamente en HTML usando innerHTML
+            const rocketDetails = document.getElementById('rocket-details');
+            if (rocketDetails) {
+                rocketDetails.innerHTML = /*html*/
+
+                `
+                <section class='up'>
+                <h2>${nameRocket}</h2>
+                </section>
+
+                <section class='down'>
+                <section class='down_left'>
+                <p><strong>description:</strong> ${description}</p>
+                <p><strong>ID:</strong> ${idRocket}</p>
+                <p><strong>country:</strong> ${country}</p>
+                <p><strong>cost_per_launch:</strong> ${cost_per_launch}</p>
+                <p><strong>company:</strong> ${company}</p>
+                <p><strong>meters_height:</strong> ${meters_height}</p>
+                <p><strong>feet_height:</strong> ${feet_height}</p>
+                <p><strong>first_flight:</strong> ${first_flight}</p>
+                <p><strong>kg_mass:</strong> ${kg_mass}</p>
+                <p><strong>lb_mass:</strong> ${lb_mass}</p>
+                <p><strong>meters_diameter:</strong> ${meters_diameter}</p>
+                <p><strong>feet_diameter:</strong> ${feet_diameter}</p>
+                <p><strong>Wikipedia:</strong> <a href="${wikipedia}">Link</a></p>
+                </section>
+
+                <section class='down_middle'>
+                <section class='down_middle_up'>
+                <section class='down_middle_up_left'>
+                <p><strong>kN_first_stage:</strong> ${kN_first_stage}</p>
+                <p><strong>lbf_first_stage:</strong> ${lbf_first_stage}</p>
+                </section>
+                <section class='down_middle_up_right'>
+                <p><strong>kN_thrust_vacuum:</strong> ${kN_thrust_vacuum}</p>
+                <p><strong>lbf_thrust_vacuum:</strong> ${lbf_thrust_vacuum}</p>
+                </section>
+                </section>
 
 
-// Imprimir dinámicamente en HTML usando innerHTML
-    const rocketDetails = document.getElementById('rocket-details');
-    if (rocketDetails) {
-        rocketDetails.innerHTML = `
-            <h2>${nombreCohete}</h2>
-            <p><strong>Descripción:</strong> ${descripción}</p>
-            <p><strong>País:</strong> ${país}</p>
-            <p><strong>Compañía:</strong> ${compañía}</p>
-            <p><strong>Altura (metros):</strong> ${alturaMetros}</p>
-            <p><strong>Altura pies:</strong>${alturaPies}</p>
-            <p><strong>Diámetro (metros):</strong> ${diametroMetros}</p>
-            <p><strong>Diametro en pies:</strong>${diametroPies}</p>
-            <p><strong>Masa (kg):</strong> ${masaKG}</p>
-            <p><strong>Masa LB:</strong>${masaLB}</p>
-            <p><strong>Empuje nivel mar:</strong>${empujeNivelMar}</p>
-            <p><strong>Empuje nivel mar Lbf:</strong>${empujeNivelMarLbf}</p>
-            <p><strong>Reutilizable:</strong>${reutilizablePrimeraEtapa}</p>
-            <p><strong>cantidad Motores</strong>${cantidadMotores}</p>
-            <p><strong>cantidadCombustible</strong>${cantidadCombustible}</p>
-            <p><strong>tiempoQuemadoPrimeraEtapa</strong>${tiempoQuemadoPrimeraEtapa}</p>
-            <p><strong>empujeSegundaEtapa</strong>${empujeSegundaEtapa}</p>
-            <p><strong>alturaCofia</strong>${alturaCofia}</p>
-            <p><strong>empujeVacio</strong>${empujeVacio}</p>
-            <p><strong>diametroCofia</strong>${diametroCofia}</p>
-            <p><strong>reutilizableSegundaEtapa</strong>${reutilizableSegundaEtapa}</p>
-            <p><strong>motoresSegundaEtapa</strong>${motoresSegundaEtapa}</p>
-            <p><strong>cantidadCombustibleSegundaEtapa</strong>${cantidadCombustibleSegundaEtapa}</p>
-            <p><strong>tiempoQuemadoSegundaEtapa</strong>${tiempoQuemadoSegundaEtapa}</p>
-            <p><strong>empujeNivelMarMotor</strong>${empujeNivelMarMotor}</p>
-            <p><strong>empujeVacíoMotor</strong>${empujeVacíoMotor}</p>
-            <p><strong>tipoMotor</strong>${tipoMotor}</p>
-            <p><strong>versiónMotor</strong>${versiónMotor}</p>
-            <p><strong>diseñoMotor</strong>${diseñoMotor}</p>
-            <p><strong>propulsante1</strong>${propulsante1}</p>
-            <p><strong>propulsante2</strong>${propulsante2}</p>
-            <p><strong>pesoCargaLEO</strong>${pesoCargaLEO}</p>
-            <p><strong>Éxito en el lanzamiento (%):</strong> ${tasaÉxito}</p>
-            <p><strong>Enlace a Wikipedia:</strong> <a href="${enlaceWikipedia}" target="_blank">Link</a></p>
-            <img src="${imagenFlickr1}"alt="Imagen del cohete ${nombreCohete}" referrerpolicy="no-referrer">
-            <img src="${imagenFlickr2}"alt="Imagen del cohete ${nombreCohete}" referrerpolicy="no-referrer">
-            <p><strong>tipoCohete</strong>${tipoCohete}</p>
-            <p><strong>activo</strong>${activo}</p>
-            <p><strong>etapas</strong>${etapas}</p>
-            <p><strong>impulsores</strong>${impulsores}</p>
-            <p><strong>costoLanzamiento</strong>${costoLanzamiento}</p>
-            <p><strong>primerVuelo</strong>${primerVuelo}</p>
-            <p><strong>idCohete</strong>${idCohete}</p>
-                `;
+                <section class='down_middle_down'>
+                <section class='down_middle_left'>
+                <p><strong>typeRocket:</strong> ${typeRocket}</p>
+                <p><strong>active:</strong> ${active}</p>
+                <p><strong>propellantCount:</strong> ${propellantCount}</p>
+                <p><strong>number_landing_legs:</strong> ${number_landing_legs}</p>
+                <p><strong>material_landing_legs:</strong> ${material_landing_legs}</p>
+                </section>
+                <section class='down_middle_middle'>
+                <img src="${imagenFlickr1}" alt="${nameRocket}" referrerpolicy="no-referrer">
+                <img src="${imagenFlickr2}" alt="${nameRocket}" referrerpolicy="no-referrer">
+               </section>
+                <section class='down_middle_right'>
+                <p><strong>type_engine:</strong> ${type_engine}</p>
+                <p><strong>engine_loss_max:</strong> ${engine_loss_max}</p>
+                <p><strong>number_engines:</strong> ${number_engines}</p>
+                <p><strong>propellant_1:</strong> ${propellant_1}</p>
+                <p><strong>propellant_2:</strong> ${propellant_2}</p>
+                </section>
+                </section>
+                </section>
+
+                <section class='down_right'>
+                <p><strong>kg_mass:</strong> ${kg_mass}</p>
+                <p><strong>lb_mass:</strong> ${lb_mass}</p>
+                <p><strong>Kg_payload_weights:</strong> ${Kg_payload_weights}</p>
+                <p><strong>lb_payload_weights:</strong> ${lb_payload_weights}</p>
+                <p><strong>meters_height:</strong> ${meters_height}</p>
+                <p><strong>feet_height:</strong> ${feet_height}</p>
+                <p><strong>meters_diameter:</strong> ${meters_diameter}</p>
+                <p><strong>feet_diameter:</strong> ${feet_diameter}</p>
+                <p><strong>meters_diameter_second_stage:</strong> ${meters_diameter_second_stage}</p>
+                <p><strong>feet_diameter_second_stage:</strong> ${feet_diameter_second_stage}</p>
+                <p><strong>meters_height_composite_fairing:</strong> ${meters_height_composite_fairing}</p>
+                <p><strong>feet_height_composite_fairing:</strong> ${feet_height_composite_fairing}</p>
+                </section>
+                </section>
+            `;
             } else {
                 console.error('Elemento con ID "rocket-details" no encontrado.');
             }
@@ -175,7 +265,6 @@ const obtenerDatosCohete = async (rocketId) => {
         console.error('Error al obtener los datos del cohete:', error);
     }
 };
-
 
 // ______________________________________________________________________________________________________________________________________________________________________________//
 
@@ -193,7 +282,7 @@ const crearGaleriaBotones = async () => {
         data.forEach((rocket, index) => {
             const button = document.createElement('button');
             button.textContent = `${index + 1}`;
-            button.onclick = () => obtenerDatosCohete(rocket.id);
+            button.onclick = () => getAllDetailDataRockets(rocket.id);
             buttonsContainer.appendChild(button);
         });
 
@@ -207,7 +296,7 @@ const crearGaleriaBotones = async () => {
 
         // Invocar la información del primer cohete automáticamente
         if (data.length > 0) {
-            obtenerDatosCohete(data[0].id); // Invocamos la información del primer cohete
+            getAllDetailDataRockets(data[0].id); // Invocamos la información del primer cohete
         }
     } else {
         console.error('Elemento con ID "buttons-container" no encontrado.');
